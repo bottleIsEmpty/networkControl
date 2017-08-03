8968a4cc73c2dbf81c6743ad195909779276c77f
Alexandr Bobko <sasha.bobcko@gmail.com>
2017-08-03 14:14:16 +0400

    first commit

---
+++ app/src/main/java/su/rck/networkcontrol/MissionListActivity.java
@@ -0,0 +1,24 @@
+package su.rck.networkcontrol;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MissionListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission_list);
+    }
+
+    @Override
+    protected Fragment createFragment() {
+        return new Fragment();
+    }
+}
